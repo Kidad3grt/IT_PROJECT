1195c777ef80d847b34b5e0805c6efaa5954d198
Alozie emmanuel <alozieemmanuel564@gmail.com>
2025-01-13 14:43:11 +0100

    before

--- sql_project/2_.top_paying_jobs_skills.sql
+++ sql_project/2_.top_paying_jobs_skills.sql
@@ -1,22 +1,20 @@
 --using CTS in mysql 
 WITH top_paying_jobs AS (
-    SELECT	
-        job_id,
-        job_title,
-        salary_year_avg,
-        name AS company_name
+    SELECT
+        job_postings_fact.job_title_short,	
+        job_postings_fact.job_id,
+        job_postings_fact.job_location,
+        job_postings_fact.job_title,
+        job_postings_fact.salary_year_avg,
+        company_dim.name AS company_name
     FROM
         job_postings_fact
-    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE
-        job_title_short = 'Data Analyst' AND 
-        job_location = 'United States' AND 
-        salary_year_avg IS NOT NULL
-    ORDER BY
-        salary_year_avg DESC
-    lIMIT 10
-    )
-    
+        job_postings_fact.job_title_short = 'Data Analyst' AND 
+        job_postings_fact.job_location = 'Anywhere' AND 
+        job_postings_fact.salary_year_avg IS NOT NULL
+)
 SELECT 
     top_paying_jobs.*, 
     skills
@@ -25,4 +23,5 @@ FROM
 INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY
-    salary_year_avg DESC;
+    salary_year_avg DESC
+limit 10;
